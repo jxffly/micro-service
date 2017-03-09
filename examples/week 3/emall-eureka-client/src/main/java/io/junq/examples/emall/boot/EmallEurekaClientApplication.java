66885f16db8c18e0dbbee39e9990b30ef50ce64b
liujunqiang <liujunqiang@xunlei.com>
2017-03-09 14:49:53 +0800

    new: 添加了eureka client调用示例

---
+++ examples/week 3/emall-eureka-client/src/main/java/io/junq/examples/emall/boot/EmallEurekaClientApplication.java
@@ -0,0 +1,30 @@
+package io.junq.examples.emall.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EmallEurekaClientApplication {
+
+	@Autowired
+	private EurekaClient client;
+	
+	@RequestMapping("/account-service")
+	public String serviceInfo(){
+		InstanceInfo instance = client.getNextServerFromEureka("account-service-eureka", false);
+		return instance.getHomePageUrl();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmallEurekaClientApplication.class, args);
+	}
+}
