d6bbc4d4cfc87172c80210d89338681e27c3ddb0
liujunqiang <liujunqiang@xunlei.com>
2017-03-16 23:00:02 +0800

    new: 添加API Gateway项目支持eureka

---
+++ examples/week4/api-gateway-zuul/src/main/java/io/junq/examples/emall/boot/gateway/ApiGatewayZuulApplication.java
@@ -0,0 +1,16 @@
+package io.junq.examples.emall.boot.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZuulApplication.class, args);
+	}
+}
--- examples/week4/order-service/src/main/java/io/junq/examples/emall/boot/order/OrderServiceApplication.java
+++ examples/week4/order-service/src/main/java/io/junq/examples/emall/boot/order/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package io.junq.examples.emall.boot.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
 @ComponentScan("io.junq.examples.emall")
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
