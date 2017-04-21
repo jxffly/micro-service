2384db664f0aeb16b3b4125695bc92278922a407
liujunqiang <liujunqiang@xunlei.com>
2017-04-21 11:39:46 +0800

    new: 添加emall-parent, hystrix-dashboard项目

--- examples/week8/cart-service/src/main/java/io/junq/examples/emall/boot/cart/CartServiceApplication.java
+++ examples/week8/cart-service/src/main/java/io/junq/examples/emall/boot/cart/CartServiceApplication.java
@@ -3,9 +3,11 @@ package io.junq.examples.emall.boot.cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class CartServiceApplication {
 
 	public static void main(String[] args) {
